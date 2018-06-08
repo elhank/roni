909cd05e28f62576db8a9c93807fd17dca653ac9
elhank <40055083+elhank@users.noreply.github.com>
2018-06-08 14:43:29 +0700

    Add files via upload

---
+++ IPTV - Copy.php
@@ -0,0 +1,72 @@
+#EXTM3U Elhank
+#EXTINF:-1,## WELCOME ELHANK GEOVANNI (SANTAI DULU BRO) ##
+http://163.172.39.167:25461/live/KodiBalkan/r2Q7LMYnNn/5.ts
+
+#EXTINF:1,LIGA1
+http://edge.telin.useetv.com/sc/useesport_720p.m3u8
+
+#EXTINF:-1,TRANS7
+https://cdn-livetv1.metube.id/hls/trans7_480/index.m3u8
+
+#EXTINF:-1,TRANSTV
+https://cdn-livetv1.metube.id/hls/transtv_480/index.m3u8
+
+#EXTINF:-1,GLOBALTV
+https://cdn-livetv2.metube.id/hls/globaltv_480/index.m3u8
+
+#EXTINF:-1,RCTI
+https://edge.okezone.com/live/rctioke.m3u8
+ 
+#EXTINF:-1,RCTI
+https://cdn-livetv2.metube.id/hls/rcti_480/index.m3u8
+
+#EXTINF:-1,MNC TV
+https://cdn-livetv2.metube.id/hls/mnctv_480/index.m3u8
+ 
+#EXTINF:-1,SCTV
+https://cdn-livetv1.metube.id/hls/sctv_480/index.m3u8
+ 
+#EXTINF:-1,INDOSIAR
+https://cdn-livetv1.metube.id/hls/indosiar_480/index.m3u8
+
+
+#EXTINF:-1,ANTV
+https://cdn-livetv1.metube.id/hls/antv_480/index.m3u8
+
+#EXTINF:-1,MNC NEWS
+https://cdn-livetv1.metube.id/hls/mncnews_480/index.m3u8
+ 
+#EXTINF:-1,CNN INDONESIA
+https://live.cnnindonesia.com/livecnn/smil:cnntv.smil/chunklist_w1705215623_b280000_sleng.m3u8
+ 
+#EXTINF:-1,CNN INDONESIA HD
+https://live.cnnindonesia.com/livecnn/smil:cnntv.smil/chunklist_w1705215623_b384000_sleng.m3u8
+
+#EXTINF:-1,TV ONE
+https://cdn-livetv1.metube.id/hls/tvone_480/index.m3u8
+
+#EXTINF:-1,KOMPASTV
+https://dmlive.vo.llnwd.net/26/dm/3/x54ozbu/live-2.m3u8
+
+#EXTINF:-1,BERITA SATU
+http://edge.linknetott.swiftserve.com/live/BsNew/amlst:bsnew/chunklist_b2121000.m3u8
+
+#EXTINF:-1,iNews
+https://cdn-livetv2.metube.id/hls/inewstv_480/index.m3u8
+
+#EXTINF:-1,TVRI
+http://ott.tvri.co.id/Content/HLS/Live/Channel(TVRINasional)/Stream(03)/index.m3u8
+
+#EXTINF:-1,METRO TV
+http://edge.metrotvnews.com:1935/live-edge/smil:metro.smil/chunklist_w1896090657_b1000000_sleng.m3u8
+ 
+#EXTINF:-1,METRO TV HD
+http://edge.metrotvnews.com:1935/live-edge/smil:metro.smil/chunklist_w1896090657_b1300000_sleng.m3u8
+
+#EXTINF:-1,TAHFIDZ TV
+rtsp://119.235.249.58:1935/tahfidztv/live
+
+
+
+
+
